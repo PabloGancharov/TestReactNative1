16a54b62e6f4af274ec83f8bfe14362b371d4365
Pablo Gancharov <pablogancharov@gmail.com>
2018-05-01 18:59:24 -0300

    agregado el text input

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, AsyncStorage } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
@@ -41,15 +41,42 @@ class DetailsScreen extends React.Component {
     static navigationOptions = {
         title: 'Detalles',
     };
+    // componentDidMount() {
+    //     AsyncStorage.getItem("myKey").then((value) => {
+    //         this.setState({ "myKey": value });
+    //     }).done();
+    // }
+    // getInitialState() {
+    //     return { };
+    // }
+
     render() {
         const { params } = this.props.navigation.state;
         const itemId = params ? params.itemId : null;
+
+        // const texto = this.state.mykey;
         return (
             <View style={{}}>
                 <Text>Cama Numero: {itemId}</Text>
+                <Text>Detalles:</Text>
+                <TextInput
+
+                    style={{ borderColor: 'gray', borderWidth: 1, backgroundColor: 'white', textAlignVertical: 'top' }}
+                    placeholder=""
+                    multiline={true}
+                    numberOfLines={20}
+                    // onChangeText={(text) => this.saveData(text)}
+                    
+                />
+
 
             </View >
         );
+    }
+
+    saveData(value) {
+        AsyncStorage.setItem("myKey", value);
+        this.setState({"myKey": value});
     }
 }
 
